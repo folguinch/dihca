70febb8acca063c0ab8a45f71d4e61572a0314ab
Fernando Olguin <f.olguin.ch@gmail.com>
2024-09-20 16:03:57 +0800

    Change eps to pdf

--- poster_plots.py
+++ poster_plots.py
@@ -12,8 +12,9 @@ if __name__ == '__main__':
         2: 'moment1',
         3: 'split_moments',
         4: 'pv_maps',
+        5: 'streamers'
     }
-    skip = [1, 2, 3]
+    skip = [1, 2, 3, 5]
 
     # Read sources from command line
     sources = ['G336.01-0.82']
@@ -29,7 +30,7 @@ if __name__ == '__main__':
             config = config_dir / f'{source}_{val}.cfg'
             if config.exists():
                 print(f'Plotting {config}')
-                plotname = figures / source / 'posters' / f'{val}.eps'
+                plotname = figures / source / 'posters' / f'{val}.pdf'
                 plotter([f'{config}', f'{plotname}'])
             elif config_dir.glob(f'{source}_{val}_*.cfg'):
                 for config in config_dir.glob(f'{source}_{val}_*.cfg'):
@@ -37,7 +38,7 @@ if __name__ == '__main__':
                     name = config.stem.split('_')
                     ind = name.index(val.split('_')[0])
                     name = '_'.join(name[ind:])
-                    plotname = figures / source / 'posters' / f'{name}.eps'
+                    plotname = figures / source / 'posters' / f'{name}.pdf'
                     plotter([f'{config}', f'{plotname}'])
             else:
                 print(f'Skipping: {config}')
