2bcabc98a5759bb66f8061e2d1d44475d482fd49
Fernando Olguin <f.olguin.ch@gmail.com>
2025-08-20 15:05:18 +0900

    Add infall section

--- plots.py
+++ plots.py
@@ -6,7 +6,7 @@ from tile_plotter.plotter import plotter
 
 from common_paths import CONFIGS, FIGURES
 
-SKIP = [1, 3, 4, 5]
+SKIP = [1, 3, 2, 5]
 PLOT_TYPE = 'papers'
 #PLOT_TYPE = 'posters'
 STEPS = {
