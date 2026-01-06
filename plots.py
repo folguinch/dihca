5aa3fc804f56a4eba9ed0c866cf5dd3be9ba877e
Fernando Olguin <f.olguin.ch@gmail.com>
2026-01-06 15:07:39 +0900

    Change borders and small fixes

--- plots.py
+++ plots.py
@@ -6,7 +6,7 @@ from tile_plotter.plotter import plotter
 
 from common_paths import CONFIGS, FIGURES
 
-SKIP = [1, 4, 3, 5]
+SKIP = [1, 2, 3, 5]
 PLOT_TYPE = 'papers'
 #PLOT_TYPE = 'posters'
 STEPS = {
