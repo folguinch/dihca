5302dab092b69ff1383344edf03f2229407387c7
Fernando Olguin <f.olguin.ch@gmail.com>
2026-01-29 10:30:43 +0900

    Change how sources are listed and iterated

--- source_pipeline_extracted.py
+++ source_pipeline_extracted.py
@@ -31,33 +31,36 @@ from common_paths import RESULTS, CONFIGS, FIGURES
 MOLECULES = ('SO',)
 
 # Source lists:
-SOURCES_970 = ('G10.62-0.38', 'G11.1-0.12', 'G11.92-0.61',
-               'G34.43+0.24MM2', 'G35.13-0.74', 'G5.89-0.37',
-               'IRAS_181511208', 'IRAS_18182-1433', 'IRDC_182231243',
-               'IRAS_181622048',
-               'W33A', 'IRAS_180891732')
-SOURCES_490 = ('G14.22-0.50_S', 'G24.60+0.08', 'G29.96-0.02',
-               'G333.12-0.56', 'G333.23-0.06', 'G333.46-0.16',
-               'G335.579-0.272', 'G335.78+0.17', 'G336.01-0.82',
-               'G34.43+0.24', 'G343.12-0.06', 'G35.03+0.35_A',
-               'G35.20-0.74_N', 'G351.77-0.54', 'IRAS_165623959',
-               'IRAS_18337-0743', 'NGC6334I', 'NGC_6334_I_N')
-WITH_LINES_PV = ('G10.62-0.38', 'G11.1-0.12', 'G11.92-0.61', 'G29.96-0.02',
-                 'G333.12-0.56', 'G333.23-0.06', 'G333.46-0.16',
-                 'G335.579-0.272', 'G335.78+0.17', 'G336.01-0.82',
-                 'G34.43+0.24', 'G343.12-0.06', 'G35.03+0.35_A', 'G35.13-0.74',
-                 'G35.20-0.74_N', 'G351.77-0.54', 'IRAS_165623959',
-                 'IRAS_180891732', 'IRAS_18182-1433', 'NGC_6334_I_N', 'W33A')
-WITH_K8 = ('G10.62-0.38_alma1a', 'G11.92-0.61_alma1a', 'G333.46-0.16_alma1',
-           'IRAS_180891732_alma1', 'IRAS_18182-1433_alma2',
-           'IRAS_18182-1433_alma3')
-SOURCES = SOURCES_970 + SOURCES_490
+#SOURCES_970 = ('G10.62-0.38', 'G11.1-0.12', 'G11.92-0.61',
+#               'G34.43+0.24MM2', 'G35.13-0.74', 'G5.89-0.37',
+#               'IRAS_181511208', 'IRAS_18182-1433', 'IRDC_182231243',
+#               'IRAS_181622048',
+#               'W33A', 'IRAS_180891732')
+#SOURCES_490 = ('G14.22-0.50_S', 'G24.60+0.08', 'G29.96-0.02',
+#               'G333.12-0.56', 'G333.23-0.06', 'G333.46-0.16',
+#               'G335.579-0.272', 'G335.78+0.17', 'G336.01-0.82',
+#               'G34.43+0.24', 'G343.12-0.06', 'G35.03+0.35_A',
+#               'G35.20-0.74_N', 'G351.77-0.54', 'IRAS_165623959',
+#               'IRAS_18337-0743', 'NGC6334I', 'NGC_6334_I_N')
+#WITH_LINES_PV = ('G10.62-0.38', 'G11.1-0.12', 'G11.92-0.61', 'G29.96-0.02',
+#                 'G333.12-0.56', 'G333.23-0.06', 'G333.46-0.16',
+#                 'G335.579-0.272', 'G335.78+0.17', 'G336.01-0.82',
+#                 'G34.43+0.24', 'G343.12-0.06', 'G35.03+0.35_A', 'G35.13-0.74',
+#                 'G35.20-0.74_N', 'G351.77-0.54', 'IRAS_165623959',
+#                 'IRAS_180891732', 'IRAS_18182-1433', 'NGC_6334_I_N', 'W33A')
+#WITH_K8 = ('G10.62-0.38_alma1a', 'G11.92-0.61_alma1a', 'G333.46-0.16_alma1',
+#           'IRAS_180891732_alma1', 'IRAS_18182-1433_alma2',
+#           'IRAS_18182-1433_alma3')
+#SOURCES = SOURCES_970 + SOURCES_490
 #SOURCES = WITH_LINES_PV
 #SOURCES = ('G10.62-0.38', 'NGC_6334_I_N')# 'G35.13-0.74')
 #SOURCES = ('IRAS_181622048',)
 #SOURCES = ('NGC6334I',)
 #SOURCES = WITH_K8
 #SOURCES = ('G333.23-0.06_alma3b', )
+SOURCES = {
+    'NGC_6334_I_N': (8,),
+}
 
 # Source-specific half widths
 HWIDTHS = {
@@ -617,9 +620,12 @@ if __name__ == '__main__':
 
     # Iterate over source config files
     config_dir = CONFIGS / 'extracted'
-    for source in sources:
-        print('Source: ', source)
-        for config in config_dir.glob(f'{source}*.cfg'):
+    for (field, hmcs) in sources.items():
+        print(f'Field: {field}')
+        for hmc in hmcs:
+            # Config file
+            config = config_dir / f'{field}_ALMAe{hmc}.cfg'
+
             # Open source
             src = Source(config_file=config)
             outdir = RESULTS / src.name / array / 'per_hot_core'
